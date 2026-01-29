02ae09c4f5566cbc55e4b0927b59ca1d13835fd2
egon-bling <egon.reinhardt@bling.com.br>
2026-01-28 22:03:08 -0300

    inicia crud

--- modulo_1/src/services/blingApi.ts
+++ modulo_1/src/services/blingApi.ts
@@ -1,3 +1,20 @@
-/* Ler o token (ou code, no seu caso didático)
-Centralizar chamadas HTTP
-Facilitar paginação */
+/* Service responsável por comunicação com a API do Bling
+Centralizar chamadas HTTP*/
+
+import axios from "axios";
+import { useAuthStore } from "../stores/auth";
+
+
+
+export function blingApi() {
+    const auth = useAuthStore()
+
+    return axios.create({
+        baseURL:'https://www.bling.com.br/Api/v3',
+        headers: {
+            Authorization: `Bearer ${auth.oauthCode}`,
+            'ContentType': 'application/json',
+        },
+    })
+
+}
